8af804ad6993a44ccc69f3a0692d9e2f01d423be
Stefan Pantic <stefan.pantic@tomtom.com>
2025-02-25 12:03:15 +0100

    test: move tests to GTest

--- project/lib_a/test/lib_a_test.cpp
+++ project/lib_a/test/lib_a_test.cpp
@@ -1,33 +1,13 @@
-#include <iostream>
-#include <cassert>
+#include <gtest/gtest.h>
 
 #include <lib_a/library.hpp>
 
-namespace test
+#include <iostream>
+#include <cassert>
+
+TEST(lib_a_test, initialization_test)
 {
-    int initialization_test()
-    {
-        lib_a::Point point(1.0, 2.0);
-
-        if (point.GetX() != 1.0)
-        {
-            std::cerr << "Error: point.GetX() != 1.0." << std::endl;
-            return 1;
-        }
-
-        if (point.GetY() != 2.0)
-        {
-            std::cerr << "Error: point.GetY() != 2.0." << std::endl;
-            return 1;
-        }
-
-        return 0;
-    }
-}
-
-int main()
-{
-    assert(test::initialization_test() == 0);
-    std::cout << "All tests passed." << std::endl;
-    return 0;
+    lib_a::Point point(1.0, 2.0);
+    EXPECT_EQ(point.GetX(), 1.0);
+    EXPECT_EQ(point.GetY(), 2.0);
 }
--- project/lib_b/test/lib_b_test.cpp
+++ project/lib_b/test/lib_b_test.cpp
@@ -1,29 +1,13 @@
-#include <iostream>
-#include <cassert>
+#include <gtest/gtest.h>
 
 #include <lib_b/library.hpp>
 
-namespace test
+#include <iostream>
+#include <cassert>
+
+TEST(lib_b_test, comparisson_test)
 {
-    int comparisson_test()
-    {
-        lib_a::Point a(1.0, 2.0);
-        lib_a::Point b(3.0, 4.0);
-
-        if (auto distance = lib_b::CalculateDistance(a, b);
-            (distance - 2.82843) > 1e-7)
-        {
-            std::cerr << "Error: CalculateDistance(a, b) = " << lib_b::CalculateDistance(a, b) << "." << std::endl;
-            return 1;
-        }
-
-        return 0;
-    }
-}
-
-int main()
-{
-    assert(test::comparisson_test() == 0);
-    std::cout << "All tests passed." << std::endl;
-    return 0;
+    lib_a::Point a(1.0, 2.0);
+    lib_a::Point b(3.0, 4.0);
+    EXPECT_NEAR(lib_b::CalculateDistance(a, b), 2.8284271247461903, 1e-7);
 }
